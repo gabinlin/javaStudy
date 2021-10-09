748c342a085f16e7c91eccb265d42312909eee43
gabin <18649660797@163.com>
2021-10-09 11:18:06 +0800

    docs: 补充说明

--- java16/src/top/gabin/java16/TestInstanceOf.java
+++ java16/src/top/gabin/java16/TestInstanceOf.java
@@ -3,6 +3,7 @@ package top.gabin.java16;
 public class TestInstanceOf {
 
     public static void main(String[] args) {
+        // String的话会有语法错误，认为没有意义（编译时）
         Object a = "a";
         // 这个语法感觉像是抄switch的。。。
         if (a instanceof String b) {
