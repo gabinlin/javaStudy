c8d6ea1c56e8c85b3ffdd11bb7e0cf6250392d28
gabin <18649660797@163.com>
2021-10-18 18:06:06 +0800

    refactor: 只保留多数据部分

--- spring-boot-mul-data-source/src/test/java/top/gabin/springBoot/ApplicationTests.java
+++ spring-boot-mul-data-source/src/test/java/top/gabin/springBoot/ApplicationTests.java
@@ -1,19 +1,15 @@
 package top.gabin.springBoot;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import top.gabin.springBoot.properties.GabinTopConfig;
 
 @SpringBootTest
 class ApplicationTests {
 
-    @Autowired
-    private GabinTopConfig gabinTopConfig;
 
     @Test
     void contextLoads() {
-        System.out.println(gabinTopConfig);
+
     }
 
 }
